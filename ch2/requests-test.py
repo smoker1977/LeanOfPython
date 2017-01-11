09cff62913ea7bdd137fde90f58561dab9180c20
Tomonori KURAHARA <t.kurahara@gmail.com>
2017-01-12 01:31:15 +0900

    表示形式の変更

--- ch2/requests-test.py
+++ ch2/requests-test.py
@@ -1,12 +1,15 @@
-# データを取得
+# 現在時刻を取得する
 import requests
 r = requests.get("http://api.aoikujira.com/time/get.php")
 
+# 対象URL
+print("対象URL：" + r.url)
+
 # テキスト形式でデータを得る
 text = r.text
-print(text)
+print("TEXT\n" + text)
 
 # バイナリ形式でデータを得る
 bin = r.content
+print("BINARY")
 print(bin)
-
